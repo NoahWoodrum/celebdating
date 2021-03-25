8da3f6b317a19f6720765642c5c555f1e2712621
Noah Woodrum <heymymomthinksimcool@gmail.com>
2021-03-25 14:16:35 -0700

    display questions and added buisness loogic for all bachelors
    Co-authored-by: Shane Graff <copellius@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  event.preventDefault();
+  $("#celebrityForm").submit(function () {
+    /*const question1 = (prompt("Do you like country or City")); */
+    const question1 = $("input#question1").val();
+    const question2 = $("input#question2").val();
+
+    if (question1 === "city" && question2 ==="no") {
+
+      $('#shrek').show();
+    }
+    else if (question1 === "city" && question2 ==="yes") {
+
+      $("#po").show();
+    }
+    else if (question1 === "country" && question2 ==="no") {
+
+      $("#megamind").show();
+    }
+    else if (question1 === "country" && question2 ==="yes") {
+
+      $("#donkey").show();
+    }
+    else {
+      
+      $("$nonsense").show();
+    }
+      /*if else
+
+      */
+
+      
+});
